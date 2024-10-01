c4b24bed61f5b77aebb2282db3cc7612a1e09e1c
KIRS1K <ilya.kirsnov@bk.ru>
2024-10-01 17:48:52 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+
+
+
+namespace ConsoleApp18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "C:\\Users\\ilyak\\OneDrive\\Рабочий стол\\1.txt";
+            StreamReader sr = new StreamReader(file);
+            string beta = sr.ReadToEnd();
+            var noPunctuationText = new string(beta.Where(c => !char.IsPunctuation(c)).ToArray());
+
+            // Разбиваем текст на слова и приводим к нижнему регистру
+            var words = noPunctuationText.ToLower().Split(new[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Подсчитываем частоту слов
+            var wordCounts = words.GroupBy(word => word)
+                                  .Select(group => new { Word = group.Key, Count = group.Count() })
+                                  .OrderByDescending(x => x.Count)
+                                  .Take(10);
+
+            // Выводим 10 наиболее частых слов
+            foreach (var word in wordCounts)
+            {
+                Console.WriteLine($"{word.Word}: {word.Count}");
+            }
+        }
+    }
+}
